d017b3f02ddbe314de8f234040b52d76a217caea
Rory Flint <roryflintphoto@gmail.com>
2020-02-27 13:50:46 +0000

    Build schema for quotes table

---
+++ database/migrations/20200227134414_createQuotesTable.js
@@ -0,0 +1,19 @@
+exports.up = function(knex) {
+  return knex.schema.createTable('quotes', table => {
+    table.increments();
+    table
+      .string('quote')
+      .notNullable()
+      .unique();
+    table.string('author').notNullable();
+    table.string('url').notNullable();
+    table
+      .integer('dayOfYear')
+      .notNullable()
+      .unsigned();
+  });
+};
+
+exports.down = function(knex) {
+  return knex.schema.dropTableIfExists('quotes');
+};
