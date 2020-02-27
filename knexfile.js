c70c776c5064c876487f6a8aa04541c7d7ea8aff
Rory Flint <roryflintphoto@gmail.com>
2020-02-27 15:24:35 +0000

    Update to postgres settings

--- knexfile.js
+++ knexfile.js
@@ -35,14 +35,14 @@ module.exports = {
       directory: './database/seeds',
     },
   },
-  // production: {
-  //   client: 'pg',
-  //   connection: process.env.DATABASE_URL,
-  //   migrations: {
-  //     directory: './database/migrations',
-  //   },
-  //   seeds: {
-  //     directory: './database/seeds',
-  //   },
-  // },
+  production: {
+    client: 'pg',
+    connection: process.env.DATABASE_URL,
+    migrations: {
+      directory: './database/migrations',
+    },
+    seeds: {
+      directory: './database/seeds',
+    },
+  },
 };
