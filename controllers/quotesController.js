47c8889d14daf0ceaa7e158d5aaeb45888da9515
Rory Flint <roryflintphoto@gmail.com>
2020-02-27 15:18:52 +0000

    Add get all quotes route added for testing

--- controllers/quotesController.js
+++ controllers/quotesController.js
@@ -1,5 +1,18 @@
 const helpers = require('../helpers/quotesHelpers');
 
+async function getAllQuotes(req, res) {
+    try {
+      const quotes = await helpers.getAllQuotes();
+      if (quotes) {
+        res.status(200).json(quotes);
+      } else {
+        res.status(404).json({ message: 'No quotes to display' });
+      }
+    } catch (error) {
+      res.status(500).json({ error });
+    }
+  }
+
 async function getQuoteByDate(req, res) {
   const { date } = req.params;
   try {
@@ -30,6 +43,7 @@ async function postNewQuote(req, res) {
 }
 
 module.exports = {
+    getAllQuotes,
     postNewQuote,
     getQuoteByDate  
 }
--- routes/quotesRoute.js
+++ routes/quotesRoute.js
@@ -3,8 +3,8 @@ const controller = require('../controllers/quotesController');
 
 const Router = express.Router();
 
+Router.get('/', controller.getAllQuotes);
 Router.get('/:date', controller.getQuoteByDate);
-
 Router.post('/', controller.postNewQuote);
 
 module.exports = Router;;
