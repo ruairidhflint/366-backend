73548be420232f62670dfa1feea4bf9c1f39decd
Rory Flint <roryflintphoto@gmail.com>
2020-03-03 19:59:13 +0000

    Clean up code

--- routes/quotesRoute.js
+++ routes/quotesRoute.js
@@ -6,7 +6,7 @@ const Router = express.Router();
 Router.get('/', controller.getAllQuotes);
 Router.get('/:date', controller.getQuoteByDate);
 
-
 // Router.post('/', controller.postNewQuote);
 // Router.put('/:id', controller.editQuote);
+
 module.exports = Router;;
